49b1d5ac51346a59090b14b0fe2a187dc7211d1e
Pavel Bugaian <pavel.bugaian@isa.utm.md>
2024-11-10 09:59:35 +0200

    feat: added "upload" module, removed build folder from VCS, added file upload endpoint

--- upload/src/main/kotlin/utm/ass/project_m/plugins/Routing.kt
+++ upload/src/main/kotlin/utm/ass/project_m/plugins/Routing.kt
@@ -1,13 +1,24 @@
 package utm.ass.project_m.plugins
 
+import io.ktor.http.ContentType
 import io.ktor.server.application.*
+import io.ktor.server.request.receiveChannel
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.cio.writeChannel
+import io.ktor.utils.io.copyAndClose
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/{id}") {
+            call.respondText(call.parameters["id"] ?: "", contentType = ContentType.Text.Plain)
+        }
+
+        post("/upload") {
+            val file = File("uploads/ktor_logo.png")
+            call.receiveChannel().copyAndClose(file.writeChannel())
+            call.respondText("A file is uploaded")
         }
     }
 }
