2ea6c0df61295bffb15e871454d01f9f844dd123
pbugaian <pbugaian@tripadvisor.com>
2024-11-15 18:30:50 +0200

    feat: second module setup with some refactoring

--- upload/src/main/kotlin/utm/ass/project_m/plugins/Routing.kt
+++ upload/src/main/kotlin/utm/ass/project_m/plugins/Routing.kt
@@ -16,41 +16,6 @@ import java.awt.image.BufferedImage
 
 fun Application.configureRouting() {
     routing {
-        var fileDescription = ""
-        var fileName = ""
-
-        post("/upload") {
-            val multipartData = call.receiveMultipart()
-
-            multipartData.forEachPart { part ->
-                when (part) {
-                    is PartData.FormItem -> {
-                        fileDescription = part.value
-                    }
-
-                    is PartData.FileItem -> {
-                        fileName = part.originalFileName as String
-                        val fileBytes = part.provider().readRemaining().readByteArray()
-                        val uploadsDirectory = File("uploads\\\\")
-                        if (!uploadsDirectory.exists()) {
-                            uploadsDirectory.mkdir()
-                        }
-                        val newFile = File(uploadsDirectory, fileName).apply {
-                            if (exists()) {
-                                call.respondText("File already exists", status = HttpStatusCode.BadRequest)
-                            }
-                        }
-                        newFile.writeBytes(fileBytes)
-                    }
-
-                    else -> {}
-                }
-                part.dispose()
-            }
-
-            call.respondText("$fileDescription is uploaded to 'uploads/$fileName'")
-        }
-
         get("download/{fileName}") {
             val fileName = call.parameters["fileName"] ?: return@get call.respondText(
                 "Provide file name",
@@ -61,29 +26,6 @@ fun Application.configureRouting() {
             File(uploadsDirectory, fileName).apply {
                 if (exists()) {
                     call.respondFile(this)
-                } else {
-                    call.respondText(
-                        "File not found",
-                        status = HttpStatusCode.NotFound
-                    )
-                }
-            }
-        }
-
-        get("process/{fileName}/mirror") {
-            val fileName = call.parameters["fileName"] ?: return@get call.respondText(
-                "Provide file name",
-                status = HttpStatusCode.BadRequest
-            )
-            val uploadsDirectory = File("uploads\\\\")
-
-            File(uploadsDirectory, fileName).apply {
-                if (exists()) {
-                    val image = javax.imageio.ImageIO.read(this)
-                    val mirroredImage = mirrorImage(image)
-                    val newFile = File(uploadsDirectory, "mirrored_$fileName")
-                    javax.imageio.ImageIO.write(mirroredImage, "png", newFile)
-                    call.respondFile(newFile)
                 } else {
                     call.respondText(
                         "File not found",
@@ -167,65 +109,4 @@ fun Application.configureRouting() {
             }
         }
     }
-}
-
-fun mirrorImage(image: BufferedImage, horizontally: Boolean = true): BufferedImage {
-    val mirrored = BufferedImage(image.width, image.height, image.type)
-    val graphics = mirrored.createGraphics()
-    val transform = if (horizontally) {
-        AffineTransform(-1.0, 0.0, 0.0, 1.0, image.width.toDouble(), 0.0)
-    } else {
-        AffineTransform(1.0, 0.0, 0.0, -1.0, 0.0, image.height.toDouble())
-    }
-    graphics.drawImage(image, transform, null)
-    graphics.dispose()
-    return mirrored
-}
-
-fun negativeImage(image: BufferedImage): BufferedImage {
-    val negative = BufferedImage(image.width, image.height, image.type)
-    val graphics = negative.createGraphics()
-    graphics.drawImage(image, 0, 0, null)
-    graphics.dispose()
-    for (x in 0 until negative.width) {
-        for (y in 0 until negative.height) {
-            val color = negative.getRGB(x, y)
-            val alpha = color shr 24 and 0xff
-            val red = 255 - color shr 16 and 0xff
-            val green = 255 - color shr 8 and 0xff
-            val blue = 255 - color and 0xff
-            val newColor = alpha shl 24 or (red shl 16) or (green shl 8) or blue
-            negative.setRGB(x, y, newColor)
-        }
-    }
-    return negative
-}
-
-fun cropImage(image: BufferedImage, x: Int, y: Int, width: Int, height: Int): BufferedImage {
-    return image.getSubimage(x, y, width, height)
-}
-
-fun sepiaImage(image: BufferedImage): BufferedImage {
-    val sepia = BufferedImage(image.width, image.height, image.type)
-    val graphics = sepia.createGraphics()
-    graphics.drawImage(image, 0, 0, null)
-    graphics.dispose()
-    for (x in 0 until sepia.width) {
-        for (y in 0 until sepia.height) {
-            val color = sepia.getRGB(x, y)
-            val alpha = color shr 24 and 0xff
-            var red = color shr 16 and 0xff
-            var green = color shr 8 and 0xff
-            var blue = color and 0xff
-            val newRed = (red * 0.393 + green * 0.769 + blue * 0.189).toInt()
-            val newGreen = (red * 0.349 + green * 0.686 + blue * 0.168).toInt()
-            val newBlue = (red * 0.272 + green * 0.534 + blue * 0.131).toInt()
-            red = if (newRed > 255) 255 else newRed
-            green = if (newGreen > 255) 255 else newGreen
-            blue = if (newBlue > 255) 255 else newBlue
-            val newColor = alpha shl 24 or (red shl 16) or (green shl 8) or blue
-            sepia.setRGB(x, y, newColor)
-        }
-    }
-    return sepia
 }
